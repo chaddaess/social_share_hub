14c1aae3aee529c7827d069676a54d1de090007a
chadda <chadha.essid@insat.ucar.tn>
2023-06-24 19:37:43 +0100

    added authentification by form

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements FixtureGroupInterface
 {
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher=$hasher;
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
 
+        $user=new User();
+        $user->setEmail("dummyUser1@gmail.com");
+        $user->setPassword($this->hasher->hashPassword($user,'1234'));
+        $manager->persist($user);
         $manager->flush();
+
+        $admin1=new User();
+        $user->setEmail("admin1@gmail.com");
+        $admin1->setRoles(["ROLE_ADMIN"]);
+        $admin1->setPassword($this->hasher->hashPassword($admin1,'admin'));
+        $manager->persist($admin1);
+        $manager->flush();
+    }
+    public static function getGroups(): array
+    {
+        return ['user'];
     }
 }
