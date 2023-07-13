9784a3c5d2bcefd36a68fc564bd1d776e083c852
chadda <chadha.essid@insat.ucar.tn>
2023-07-13 08:49:06 +0100

    additional registration test (accept terms test)

--- tests/Integration/RegistrationTest.php
+++ tests/Integration/RegistrationTest.php
@@ -7,7 +7,7 @@ use App\Repository\UserRepository;
 
 class RegistrationTest extends WebTestCase
 {
-    public function testRegister()
+    public function testUserShouldBeRegistered()
     {
         $client = static::createClient();
         // Submit the registration form with valid data
@@ -18,16 +18,43 @@ class RegistrationTest extends WebTestCase
         'registration_form[agreeTerms]' => true
         ]);
         $client->submit($form);
-
         // Check that the response is a redirect to the login page
         $response = $client->getResponse();
         $this->assertTrue($response->isRedirect('/login'));
-
         // Check that a new user was added to the database
         $entityManager = self::$kernel->getContainer()->get('doctrine.orm.entity_manager');
         $user = $entityManager->getRepository(User::class)->findOneBy([
             'email' => 'test@test.com'
         ]);
         $this->assertNotNull($user);
+        $entityManager->remove($user);
+        $entityManager->flush();
     }
+
+    public function testUserShouldNOTBeRegistered()
+    {
+        //user should NOT be registered if they don't accept terms
+        $client = static::createClient();
+        // Submit the registration form with valid data
+        $crawler = $client->request('GET', '/register');
+        $form = $crawler->selectButton('sign up')->form([
+            'registration_form[email]' => 'test@test.com',
+            'registration_form[plainPassword]' => 'password123',
+            'registration_form[agreeTerms]' => false
+        ]);
+        $client->submit($form);
+        // Check that the response is a redirect to the login page
+        $response = $client->getResponse();
+        $this->assertFalse($response->isRedirect('/login'));
+        // Check that a new user was added to the database
+        $entityManager = self::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $user = $entityManager->getRepository(User::class)->findOneBy([
+            'email' => 'test@test.com'
+        ]);
+        $this->assertNull($user);
+    }
+
+
+
+
 }
